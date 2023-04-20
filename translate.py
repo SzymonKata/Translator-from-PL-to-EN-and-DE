c155e54058c3f41380b7927cc167fcc24882573e
Szymon <107206532+SzymonKata@users.noreply.github.com>
2023-04-20 21:07:24 +0200

    Update translate.py

--- translate.py
+++ translate.py
@@ -5,7 +5,7 @@ def translate_word():
     slowo = entry.get()
     if slowo.lower() == 'q':
         output.config(text="Wyszedłeś z programu.")
-        root.destroy()  # Close the Tkinter window
+        root.destroy() 
     else:
         try:
             tlumaczenie_en = translate(slowo, "en")
